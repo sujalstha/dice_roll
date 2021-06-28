38777aead5f41aea0e5bfd7520c52b78f984a21b
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-28 15:31:10 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #  random dice roll
---------------
-|            |
-|      *     |
-|            |
-|____________|
+
+import pygame, random, time
+
+size = 256  # Size of window/dice
+size_of_spots = size // 10
+
+d = pygame.display.set_mode((size, size))
+pygame.display.set_caption("Dice Simulator")
