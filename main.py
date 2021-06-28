7afb4f9b8c8fcaf9af52329e3cc8e756a251e19c
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-28 15:47:19 -0500

    Initial commit

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import pygame, random, time
 
 size = 256  # Size of window/dice
 size_of_spots = size // 10
+mid = int(size / 2)
+left = t = int(size / 4)
+right = b = size - left
+rolling = 12
+diecol = (255, 255, 127)
+spotcol = (0, 127, 127)
 
 d = pygame.display.set_mode((size, size))
 pygame.display.set_caption("Dice Simulator")
