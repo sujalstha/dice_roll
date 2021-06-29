96055c33f220f7f5fd660f5be3e2d9918664b3be
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-29 14:55:40 -0500

    Initial commit

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ for i in range(rolling_time):
     n = random.randint(1, 6)
     d.fill(dice_color)
     if n % 2 == 1:
-        pygame.draw.circle(d, spotcolor, (mid, mid), size_of_spots)
+        pygame.draw.circle(d, spot_color, (mid, mid), size_of_spots)
     if n == 2 or n == 3 or n == 4 or n == 5 or n == 6:
-        pygame.draw.circle(d, spotcolor, (left, b), size_of_spots)  # left bottom
-        pygame.draw.circle(d, spotcol, (right, t), size_of_spots)  # right top
+        pygame.draw.circle(d, spot_color, (left, b), size_of_spots)  # left bottom
+        pygame.draw.circle(d, spot_color, (right, t), size_of_spots)  # right top
     if n == 4 or n == 5 or n == 6:
-        pygame.draw.circle(d, spotcol, (left, t), size_of_spots)  # left top
-        pygame.draw.circle(d, spotcol, (right, b), size_of_spots)  # right bottom
+        pygame.draw.circle(d, spot_color, (left, t), size_of_spots)  # left top
+        pygame.draw.circle(d, spot_color, (right, b), size_of_spots)  # right bottom
     if n == 6:
-        pygame.draw.circle(d, spotcol, (mid, b), size_of_spots)  # middle bottom
-        pygame.draw.circle(d, spotcol, (mid, t), size_of_spots)  # middle top
+        pygame.draw.circle(d, spot_color, (mid, b), size_of_spots)  # middle bottom
+        pygame.draw.circle(d, spot_color, (mid, t), size_of_spots)  # middle top
 
     pygame.display.flip()
     time.sleep(.3)
