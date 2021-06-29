c8adffebdde9f5d78977e95940c2d43003872a9e
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-29 14:55:11 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #  random infinite dice roll
 
-import pygame, random, time
+from sys import exit
+import pygame
+import random
+import time
 
 size = 256  # Size of window/dice
 size_of_spots = size // 10
@@ -9,19 +12,19 @@ left = t = int(size / 4)
 right = b = size - left
 rolling_time = random.randrange(4, 12)
 print(rolling_time)
-diecol = (255, 255, 127)
-spotcol = (0, 127, 127)
+dice_color = (255, 255, 127)
+spot_color = (0, 127, 127)
 
 d = pygame.display.set_mode((size, size))
 pygame.display.set_caption("Dice Simulator")
 
 for i in range(rolling_time):
     n = random.randint(1, 6)
-    d.fill(diecol)
+    d.fill(dice_color)
     if n % 2 == 1:
-        pygame.draw.circle(d, spotcol, (mid, mid), size_of_spots)
+        pygame.draw.circle(d, spotcolor, (mid, mid), size_of_spots)
     if n == 2 or n == 3 or n == 4 or n == 5 or n == 6:
-        pygame.draw.circle(d, spotcol, (left, b), size_of_spots)  # left bottom
+        pygame.draw.circle(d, spotcolor, (left, b), size_of_spots)  # left bottom
         pygame.draw.circle(d, spotcol, (right, t), size_of_spots)  # right top
     if n == 4 or n == 5 or n == 6:
         pygame.draw.circle(d, spotcol, (left, t), size_of_spots)  # left top
@@ -32,3 +35,9 @@ for i in range(rolling_time):
 
     pygame.display.flip()
     time.sleep(.3)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
